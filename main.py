61e5c4071c6fdfe9871387289c459d6070f1ce96
mweissgerber1 <weissgerbermj12@gmail.com>
2022-10-21 23:16:01 -0400

    Added Machine Learning libraries

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello Wordl!")
+class generateUser:
+    def __init__():
+        generateWebsite()
+        gatherInfo()
+    
+    def generateWebsite():
+        x = 1
+
+    def gatherInfo():
+        x = 1
+
+    
